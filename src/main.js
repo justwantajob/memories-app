4509a589d480f20a329fed2c839278cb367ca1e6
gurur <kisaoglugurur@gmail.com>
2023-12-29 14:48:26 +0300

    memories app version 1

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css'
+import '../styles/components/story-section.css'
+import '../styles/components/post-section.css'
+import '../styles/components/app-menu.css'
+import '../styles/utils.css';
